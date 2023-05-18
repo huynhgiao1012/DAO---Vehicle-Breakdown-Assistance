ff9fc2853165a5613a8dcffcb61e57f10504fc6f
huynhgiao1012 <huynhgiaolethi0@gmail.com>
2023-05-18 11:36:17 +0700

    update FE

--- FE/src/screens/GarageScreen.js
+++ FE/src/screens/GarageScreen.js
@@ -1,10 +1,186 @@
-import {View, Text} from 'react-native';
+import {View, Text, ScrollView, Image, StyleSheet} from 'react-native';
 import React from 'react';
-
+import {themeColors} from '../theme';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import ServiceList from '../screens/ServiceList';
+import Rating from './Rating';
 export default function GarageScreen() {
   return (
-    <View>
-      <Text>GarageScreen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.headerView}>
+        <View>
+          <Image
+            source={require('../../assets/images/garage.jpg')}
+            style={styles.image}
+          />
+          <Rating />
+        </View>
+        <View
+          style={{
+            paddingLeft: 20,
+            width: 200,
+          }}>
+          <Text
+            style={{
+              fontSize: 26,
+              fontWeight: 'bold',
+              color: themeColors.primaryColor,
+            }}>
+            ABC GARAGE
+          </Text>
+          <View style={styles.iconTextHeader}>
+            <Icon
+              name="phone-square"
+              size={20}
+              color={themeColors.blue}
+              style={{marginRight: 8}}
+            />
+            <Text
+              style={{
+                fontSize: 14,
+                fontWeight: 'bold',
+                color: themeColors.blue,
+                fontStyle: 'italic',
+              }}>
+              0235678929
+            </Text>
+          </View>
+          <View style={styles.iconTextHeader}>
+            <Icon
+              name="map-marker"
+              size={25}
+              color={themeColors.blue}
+              style={{marginRight: 8}}
+            />
+            <Text
+              style={{
+                fontSize: 14,
+                fontWeight: 'bold',
+                color: themeColors.blue,
+                fontStyle: 'italic',
+              }}>
+              Số 110 Lạc Long Quân, Tây Hồ, Hà Nội
+            </Text>
+          </View>
+          <View style={styles.iconTextHeader}>
+            <Icon
+              name="envelope"
+              size={16}
+              color={themeColors.blue}
+              style={{marginRight: 8}}
+            />
+            <Text
+              style={{
+                fontSize: 14,
+                fontWeight: 'bold',
+                color: themeColors.blue,
+                fontStyle: 'italic',
+              }}>
+              info@otohathanh.com
+            </Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.info}>
+        <View style={styles.title}>
+          <Icon
+            name="info-circle"
+            size={22}
+            color={themeColors.blue}
+            style={{marginRight: 8}}
+          />
+          <Text
+            style={{
+              fontSize: 18,
+              fontWeight: 'bold',
+              color: themeColors.blue,
+            }}>
+            INFORMATION
+          </Text>
+        </View>
+        <Text
+          style={{
+            paddingHorizontal: 20,
+            fontSize: 14,
+            fontStyle: 'italic',
+            color: themeColors.gray60,
+          }}>
+          Hà Thành có đội ngũ nhân viên chăm sóc khách hàng và kỹ thuật có nhiều
+          kinh nghiệm trong lĩnh vực. Hà Thành có diện tích gara rất rộng thông
+          thoáng, sạch sẽ với đầy đủ trang thiết bị, hệ thống máy móc hiện đại
+          đáp như nhu cầu phục vụ cho khách hàng. Sau khi sơn xe xong, Hà Thành
+          còn hướng dẫn khách hàng cách chăm sóc xe khi sử dụng để đảm bảo lớp
+          sơn được mới lâu hơn. Bên cạnh đó thợ ở đây cũng sẽ tiến hành bọc, che
+          chắn phần vỏ ngoài xe bằng lớp nilon dán xe chuyên dụng.
+        </Text>
+        <View style={styles.title}>
+          <Icon
+            name="car"
+            size={22}
+            color={themeColors.blue}
+            style={{marginRight: 8}}
+          />
+          <Text
+            style={{
+              fontSize: 18,
+              fontWeight: 'bold',
+              color: themeColors.blue,
+            }}>
+            SERVICE
+          </Text>
+        </View>
+        <ServiceList />
+        <View style={styles.title}>
+          <Icon
+            name="gratipay"
+            size={22}
+            color={themeColors.blue}
+            style={{marginRight: 8}}
+          />
+          <Text
+            style={{
+              fontSize: 18,
+              fontWeight: 'bold',
+              color: themeColors.blue,
+            }}>
+            FEEDBACK
+          </Text>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  headerView: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    borderColor: themeColors.black,
+    borderBottomWidth: 1,
+    marginHorizontal: 20,
+    paddingVertical: 20,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 15,
+    borderColor: themeColors.primaryColor,
+    borderWidth: 5,
+  },
+  iconTextHeader: {
+    display: 'flex',
+    flexDirection: 'row',
+    paddingVertical: 3,
+    alignItems: 'center',
+  },
+
+  title: {
+    marginHorizontal: 20,
+    marginVertical: 8,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+});
---
+++ FE/src/screens/ServiceList.js
@@ -0,0 +1,141 @@
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  TextInput,
+  StyleSheet,
+  Dimensions,
+  ScrollView,
+  FlatList,
+} from 'react-native';
+import React from 'react';
+import {themeColors} from '../theme';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {useNavigation} from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+// subscribe for more videos like this :)
+export default function ServiceList() {
+  const navigation = useNavigation();
+  const data = [
+    {
+      name: 'Jonh Garage',
+      price: 20,
+      id: 1,
+    },
+    {
+      name: 'Jonh Garage',
+      price: 20,
+      id: 2,
+    },
+    {
+      name: 'Jonh Garage',
+      price: 20,
+      id: 3,
+    },
+    {
+      name: 'Jonh Garage',
+      price: 20,
+      id: 4,
+    },
+    {
+      name: 'Jonh Garage',
+      price: 20,
+      id: 5,
+    },
+    {
+      name: 'Jonh Garage',
+      price: 20,
+      id: 6,
+    },
+    {
+      name: 'Jonh Garage',
+      price: 20,
+      id: 7,
+    },
+    {
+      name: 'Jonh Garage',
+      price: 20,
+      id: 8,
+    },
+    {
+      name: 'Jonh Garage',
+      price: 20,
+      id: 9,
+    },
+    {
+      name: 'Jonh Garage',
+      price: 20,
+      id: 10,
+    },
+  ];
+  return (
+    <FlatList
+      horizontal={true}
+      style={{marginHorizontal: 20}}
+      ItemSeparatorComponent={
+        Platform.OS !== 'android' &&
+        (({highlighted}) => (
+          <View style={[styles.separator, highlighted && {marginLeft: 0}]} />
+        ))
+      }
+      data={data}
+      renderItem={({item, index, separators}) => (
+        <TouchableOpacity
+          key={item.id}
+          onPress={() => navigation.navigate('GarageDetail')}>
+          <View
+            style={{
+              width: 100,
+              height: 100,
+              paddingHorizontal: 3,
+              paddingVertical: 10,
+              borderWidth: 1,
+              borderColor: themeColors.primaryColor,
+              backgroundColor: themeColors.primaryColor,
+              marginVertical: 5,
+              borderRadius: 20,
+              marginRight: 10,
+            }}>
+            <Icon
+              name="wrench"
+              size={30}
+              color={themeColors.blue}
+              style={{alignSelf: 'center'}}
+            />
+            <Text
+              style={{
+                paddingVertical: 5,
+                fontWeight: '900',
+                fontSize: 14,
+                color: themeColors.blue,
+                textAlign: 'center',
+              }}>
+              {item.name}
+            </Text>
+            <Text
+              style={{
+                fontSize: 14,
+                color: themeColors.white,
+                textAlign: 'center',
+                paddingBottom: 10,
+                fontWeight: '600',
+              }}>
+              {item.price}$
+            </Text>
+          </View>
+        </TouchableOpacity>
+      )}
+    />
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+  item: {
+    backgroundColor: themeColors.white,
+  },
+});
