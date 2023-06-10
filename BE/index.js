289294d8427cfa33d7058eaf1e88ea83f8579fff
huynhgiao1012 <huynhgiaolethi0@gmail.com>
2023-06-10 23:41:49 +0700

    update BE

--- BE/index.js
+++ BE/index.js
@@ -26,15 +26,48 @@ app.use("/api/v1/service", serviceRoutes);
 app.use("/api/v1/user", userRoutes);
 app.use(catchError);
 const port = process.env.PORT || 3000;
+
+let onlineUsers = [];
+
+const addNewUser = (username, socketId) => {
+  !onlineUsers.some((user) => user.username === username) &&
+    onlineUsers.push({ username, socketId });
+};
+
+const removeUser = (socketId) => {
+  onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (username) => {
+  return onlineUsers.find((user) => user.username === username);
+};
+
 io.on("connection", (socket) => {
-  console.log("a user connected" + " " + socket.id);
-  socket.on("send_message", (data) => {
-    io.emit("receive_message", data);
+  socket.on("newUser", (username) => {
+    addNewUser(username, socket.id);
   });
+  console.log(onlineUsers);
+  socket.on("sendNotification", ({ senderName, receiverName, text }) => {
+    const receiver = getUser(receiverName);
+    io.to(receiver.socketId).emit("getNotification", {
+      senderName,
+      text,
+    });
+  });
+
+  socket.on("sendText", ({ senderName, receiverName, text }) => {
+    const receiver = getUser(receiverName);
+    io.to(receiver.socketId).emit("getText", {
+      senderName,
+      text,
+    });
+  });
+
   socket.on("disconnect", () => {
-    console.log("user disconnected");
+    removeUser(socket.id);
   });
 });
+
 server.listen(port, () => {
   console.log(`Server is running on PORT ${port}`);
 });
